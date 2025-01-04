b935b8c226bc1da0965d2cf8ea703b1b7188d35d
mpatel360 <mpatel360@gatech.edu>
2025-01-04 15:56:54 -0500

    Feature/initial deploy (#3)
    * deleted (commented out) the pocketbase service setup job
    
    * switched to vitejs, saving work here
    
    * saving progress on remix tutorial
    
    * some large diff

--- src/app/notes/[id]/error.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function Error() {
-  return <p>Sorry, we ran into a problem...</p>;
-}
--- src/app/projects/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function ProjectsPage() {
-  return <h1>My profile</h1>;
-}
